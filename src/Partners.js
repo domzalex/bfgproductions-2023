240a47308f65622820dd2f8b3b00df39e02ff5e3
domzalex <alexdomzalski@gmail.com>
2023-05-01 16:09:06 -0400

    fixed partner links

--- src/Partners.js
+++ src/Partners.js
@@ -6,19 +6,39 @@ import indio from './img/indio_logo.png'
 
 function Partners() {
     return (
-        <div className='partners-container'>
+        <div className="partners-container">
             <h1>B.F.G. Proudly Partners With</h1>
-            <div className='partners-banner'>
-                <a className="partners-grid-item" target="_blank" rel="noreferrer" href="https://www.spiritsusa.com">
-                    <img src={tincup} alt="Tin Cup Grill logo" />   
+            <div className="partners-banner">
+                <a
+                    className="partners-grid-item"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://tincupgrill.com"
+                >
+                    <img src={tincup} alt="Tin Cup Grill logo" />
                 </a>
-                <a className="partners-grid-item" target="_blank" rel="noreferrer" href="https://tincupgrill.com">
+                <a
+                    className="partners-grid-item"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.spiritsusa.com"
+                >
                     <img src={legends} alt="Legends Distillery logo" />
                 </a>
-                <a className="partners-grid-item" target="_blank" rel="noreferrer" href="https://www.indiobrewing.com">
+                <a
+                    className="partners-grid-item"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.indiobrewing.com"
+                >
                     <img src={indio} alt="Indio Brewing logo" />
                 </a>
-                <a className="partners-grid-item" target="_blank" rel="noreferrer" href="https://www.facebook.com/Wrightsfishandchips/">
+                <a
+                    className="partners-grid-item"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.facebook.com/Wrightsfishandchips/"
+                >
                     <img src={wrights} alt="Wright's On The Green logo" />
                 </a>
             </div>
--- src/Staff.js
+++ src/Staff.js
@@ -2,14 +2,34 @@ import React from 'react'
 
 function Staff() {
     return (
-        <div className='staff-container'>
+        <div className="staff-container">
             <div className="staff-card">
-                <div className="staff-image"></div>
-                <div className='staff-card-info'>
+                <div className="staff-image dan"></div>
+                <div className="staff-card-info">
+                    <h1>Daniel Henderson</h1>
+                    <p>Co-Owner, CEO</p>
+                </div>
+            </div>
+            <div className="staff-card">
+                <div className="staff-image chad"></div>
+                <div className="staff-card-info">
+                    <h1>Chad Honea</h1>
+                    <p>Co-Owner</p>
+                </div>
+            </div>
+            <div className="staff-card">
+                <div className="staff-image alex"></div>
+                <div className="staff-card-info">
                     <h1>Alex Domzalski</h1>
                     <p>Web Developer</p>
                 </div>
-                
+            </div>
+            <div className="staff-card">
+                <div className="staff-image haleigh"></div>
+                <div className="staff-card-info">
+                    <h1>Haleigh Crochet</h1>
+                    <p>Social Media Manager</p>
+                </div>
             </div>
         </div>
     )
